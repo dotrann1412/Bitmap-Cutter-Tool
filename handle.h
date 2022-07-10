b9f0d25098163d33ebe6711bbfb4cc37fdc1852f
dotrann1412 <dotrann.1412@gmail.com>
2022-07-10 11:02:51 +0700

    add handle.h

---
+++ handle.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+#include "bitmap.h" //24 bits format
+
+using namespace std;
+
+void handle(int, char**);
+int parseInt(char[]); 
+bool checkNum(char[]); 
